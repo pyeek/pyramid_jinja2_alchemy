b22b5c0836ae34495e8fe4d6f2da6f8327735598
Andrew Kou <me@pyeek.com>
2013-05-07 09:04:37 -0700

    Added some basic comments

--- pyramid_jinja2_alchemy/scaffolds/__init__.py
+++ pyramid_jinja2_alchemy/scaffolds/__init__.py
@@ -2,5 +2,8 @@ from pyramid.scaffolds import PyramidTemplate
 
 
 class Jinja2AlchemyProjectTemplate(PyramidTemplate):
+    """
+    Project template for jinja2 and sqlalchemy. This is only compatible with 1.3
+    """
     _template_dir = 'jinja2_alchemy'
     summary = 'Jinja2 sqlalchemy project template'
